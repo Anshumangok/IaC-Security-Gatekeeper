bca2f6403fe58b6a353818dcb416072809f4981f
Anshuman Gokhale <anshumangok@gmail.com>
2025-06-18 15:05:30 +0530

    💣 Add known vulnerable S3 config to test Checkov

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,9 @@
-resource "aws_s3_bucket" "example" {
-   acl    = "public-read"
-   bucket = "my-insecure-public-bucket" # ❌ Misconfigured
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "my-public-bucket"
+  acl    = "public-read"  # ❌ This is insecure and will be flagged
+
+  tags = {
+    Name        = "Public Bucket"
+    Environment = "Dev"
+  }
 }
