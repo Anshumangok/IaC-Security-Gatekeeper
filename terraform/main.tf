74361594549eae3ed3159f5e6b1b2cc326806742
Anshuman Gokhale <anshumangok@gmail.com>
2025-06-19 15:19:45 +0530

    ✅ Final Checkov scan setup with artifact upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # Triggering Checkov scan
 resource "aws_s3_bucket" "public_bucket" {
-  bucket = "my-test-public-bucket-v5"
+  bucket = "my-test-public-bucket-v5" #Misconfigured
   acl    = "public-read"
 
   tags = {
