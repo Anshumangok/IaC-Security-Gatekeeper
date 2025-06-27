08151c96f74bdc9662e33e5eb10cfddf39a8f257
Anshuman Gokhale <anshumangok@gmail.com>
2025-06-27 11:03:17 +0530

    Fix s3 buckets

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,18 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-public-bucket"
-  acl    = "public-read"
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+# 1. Public S3 bucket with public-read ACL (CKV_AWS_20)
+resource "aws_s3_bucket" "public_bucket_1" {
+  bucket = "my-public-test-bucket-123456"
 }
