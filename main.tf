17a3b8cb9c52860a8a915673058ffadb512443a9
Anshuman Gokhale <anshumangok@gmail.com>
2025-08-11 02:47:49 +0530

    Checkov scan

--- main.tf
+++ main.tf
@@ -1,18 +1,5 @@
-provider "aws" {
-  region = "us-east-1"
+# PUBLIC S3 bucket
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "my-public-bucket"
+  acl    = "public-read"
 }
-
-# Insecure S3 bucket: triggers CKV_AWS_21, CKV_AWS_145, CKV2_AWS_6, CKV2_AWS_62, CKV_AWS_144, CKV_AWS_18, CKV2_AWS_61
-resource "aws_s3_bucket" "secure_bucket" {
-  bucket = "my-insecure-secure-bucket"
-  acl    = "private"
-}
-
-# Intentionally missing:
-# - versioning
-# - kms encryption
-# - public access block
-# - event notifications
-# - cross-region replication
-# - access logging
-# - lifecycle configuration
